cabe6878c94541a17f50fd66a8c8d60f8441dbc9
John David Dela Cruz <johndaviddc1@gmail.com>
2023-06-21 15:07:38 +0800

    bottle song

---
+++ src/BottleSong.java
@@ -0,0 +1,27 @@
+
+public class BottleSong {
+
+	public static void main(String[] args) {
+		int bottlesNum = 10;
+		String word = "bottles";
+		
+		while (bottlesNum > 0) {
+			if (bottlesNum == 1) {
+				word = "bottle";
+			}
+			System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+			System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+			System.out.println("And if one green bottle should accidentally fall,");
+			bottlesNum--;
+			
+			if (bottlesNum > 0) {
+				System.out.println("There'll be " + bottlesNum +
+						" green " + word + ", hanging on wall");
+			} else {
+				System.out.println("There'll be no green bottles, hanging on the wall");
+			}
+		}
+
+	}
+
+}
